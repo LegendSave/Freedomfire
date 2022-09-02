84e1ab87bae9ce1cbb144ed4f6d3943d8558cd00
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-09-01 23:40:29 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+let angle = 0;
+function setup() {
+  // put setup code here
+  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+}
+function draw() {
+  // put drawing code here
+  background(51);
+  fill(255, 0, 150);
+  rotateY(angle);
+  rotateZ(angle);
+  rectMode(CENTER);
+  sphere(150);
+  angle += 0.07;
+}
