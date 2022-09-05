060319f5baf9374eb036749d80d4daa11f33418f
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-09-05 00:07:11 -0700

    f

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function draw() {
   background(51);
   stroke("white");
   noFill();
-  cam.setPosition(300, -200, charz);
+  cam.setPosition(-250, -200, charz);
   cam.lookAt(300, 250, charz);
   plane(300, 300);
   angle += 0.007;
