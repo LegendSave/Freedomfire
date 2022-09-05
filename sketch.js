e8153db60de848856e5e4f6311bd89c43502a65e
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-09-05 00:03:55 -0700

    s

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let angle = 0;
 let xCan = window.innerWidth;
 let yCan = window.innerHeight;
 let charz = 300;
+let charx = 300;
 function setup() {
   // put setup code here
   createCanvas(xCan, yCan, WEBGL);
@@ -19,16 +20,22 @@ function draw() {
   background(51);
   stroke("white");
   noFill();
-  cam.setPosition(-250, -200, charz);
-  cam.lookAt(300, 250, charz);
+  cam.setPosition(charx, -200, charz);
+  cam.lookAt(charx, 250, charz);
   plane(300, 300);
   angle += 0.007;
-  translate(300, 250, charz);
+  translate(charx, 250, charz);
   sphere(100, 20, 20);
   if(keyIsDown(65)){
     charz -= 8;
   }
   if(keyIsDown(68)){
     charz += 8;
+  }
+  if(keyIsDown(87)){
+    charx -= 8;
+  }
+  if(keyIsDown(83)){
+    charx += 8;
   }
 }
