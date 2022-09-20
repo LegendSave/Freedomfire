2eb62feb15a48e4706e8b088e36205ee7e19f086
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-09-20 09:54:10 -0700

    s

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function draw() {
   noStroke();
   fill("skyblue");
   cam.setPosition(-250, -200, -300);
-  cam.lookAt(300, 250, charz);
+  cam.lookAt(mouseY, mouseX, charz);
   angle += 0.007;
   plane(300, 300);
   translate(charx, 250, charz);
