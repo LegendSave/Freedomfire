b3777abed1634b55c84815811fc0bb4b0f54bc59
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-09-20 10:52:43 -0700

    s

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ let xCan = window.innerWidth;
 let yCan = window.innerHeight;
 let charz = 300;
 var charx = 300;
-document.onload(fullscreen());
+window.onload (e => {
+  fullscreen();
+});
 function setup() {
   // put setup code here
   createCanvas(xCan, yCan, WEBGL);
